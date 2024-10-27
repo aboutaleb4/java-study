68085a1c0917029a73b18d9467db2328db64041f
ahmed.aboutaleb <ahmed.aboutaleb@talabat.com>
2024-10-27 10:26:32 +0300

    more playing around with strings, chars, and StringBuilders

--- StringAndCharNotes.java
+++ StringAndCharNotes.java
@@ -23,5 +23,36 @@ public class StringAndCharNotes {
         System.out.println((char)('0' + ('9' - '0' + 1) % 10)); // returns char: '0'
         System.out.println((char)('0' + ('8' - '0' + 1) % 10)); // returns char: '9'
 
+        String stringForTesting;
+        StringBuilder stringBuilderForTesting;
+
+        stringBuilderForTesting = new StringBuilder();
+        stringBuilderForTesting.append(2);
+        stringBuilderForTesting.append(2.5);
+        stringBuilderForTesting.append('a');
+        stringBuilderForTesting.append('b');
+        stringBuilderForTesting.append('c');
+        stringBuilderForTesting.append("xyz");
+
+        System.out.println(stringBuilderForTesting);
+
+        stringForTesting = stringBuilderForTesting.toString();
+
+        for(int i = 0; i < stringBuilderForTesting.length(); i++) {
+            System.out.println(stringBuilderForTesting.charAt(i));
+        }
+
+        stringBuilderForTesting = new StringBuilder(stringForTesting);
+        stringForTesting = stringBuilderForTesting.reverse().toString();
+
+        System.out.println(stringForTesting);
+
+        stringBuilderForTesting = new StringBuilder(30);
+        for(int i = 0; i < 40; i++) {
+            stringBuilderForTesting.append(i);
+        }
+        stringBuilderForTesting.setCharAt(20, 'a');
+        System.out.println(stringBuilderForTesting);
+        System.out.println(stringBuilderForTesting.length());
     }
 }

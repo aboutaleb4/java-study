2bafb0721d85892bf3d5c28f04a9cc001ec8c9c9
ahmed.aboutaleb <ahmed.aboutaleb@talabat.com>
2025-01-10 20:51:15 +0200

    Revert "dummy names used"
    This reverts commit de147edc2f488f672bbb93917a7d83ae4c685edd.

--- Main.java
+++ Main.java
@@ -1,44 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        StrategyA strategyA = new StrategyA();
-        StrategyB strategyB = new StrategyB();
-
-        Context context = new Context();
-
-        context.setStrategy(strategyA);
-        context.doSomething();
-
-        context.setStrategy(strategyB);
-        context.doSomething();
-    }
-}
-
-class Context {
-    Strategy strategy;
-
-    public void setStrategy(Strategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public void doSomething() {
-        strategy.execute();
-    }
-}
-
-interface Strategy {
-    public void execute();
-}
-
-class StrategyA implements Strategy {
-    @Override
-    public void execute() {
-        System.out.println(this.getClass().getName() + " has been executed");
-    }
-}
-
-class StrategyB implements Strategy {
-    @Override
-    public void execute() {
-        System.out.println(this.getClass().getName() + " has been executed");
+        System.out.println("Hello World!");
     }
 }

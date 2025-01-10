12eb2037a13e7dc1e8926e1854828fadaa555918
ahmed.aboutaleb <ahmed.aboutaleb@talabat.com>
2025-01-10 19:52:07 +0200

    AhmedWahedBas

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        AhmedWahedBas ahmedWahedBas = AhmedWahedBas.getAhmed();
+        AhmedWahedBas ahmedTany = AhmedWahedBas.getAhmed();
+
+
+        ahmedWahedBas.name = "Ahmed Aboutaleb";
+
+        System.out.println(ahmedTany.name);
+    }
+}
+
+class AhmedWahedBas {
+    public String name;
+
+    private static AhmedWahedBas ahmedWahedBas;
+
+    private AhmedWahedBas() {
+
+    }
+
+    public static AhmedWahedBas getAhmed() {
+        if(ahmedWahedBas == null) {
+            ahmedWahedBas = new AhmedWahedBas();
+        }
+
+        return ahmedWahedBas;
     }
 }

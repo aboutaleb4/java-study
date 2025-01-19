b28ef9bad96d44f37451cce7f0eec8a870a53174
ahmed.aboutaleb <ahmed.aboutaleb@talabat.com>
2025-01-19 13:17:30 +0200

    file system dummy project

--- oop/interfaces_and_abstract_classes/Demo.java
+++ oop/interfaces_and_abstract_classes/Demo.java
@@ -2,7 +2,21 @@ package oop.interfaces_and_abstract_classes;
 
 public class Demo {
     public static void main(String[] args) {
+        FileSystem fileSystem = new FileSystem();
 
+        fileSystem.ls("");
+        fileSystem.mkdir("a");
+        fileSystem.ls("");
+        fileSystem.mkdir("a/b/c");
+        fileSystem.ls("a/b/c");
+        fileSystem.ls("a/b/");
+        fileSystem.ls("a/b");
+        fileSystem.touch("a/b/d");
+        fileSystem.ls("a/b");
+        fileSystem.addContent("a/b/d", "Ahmed ");
+        fileSystem.getContent("a/b/d");
+        fileSystem.addContent("a/b/d", "Aboutaleb");
+        fileSystem.getContent("a/b/d");
     }
 }
 
--- oop/interfaces_and_abstract_classes/FileSystem.java
+++ oop/interfaces_and_abstract_classes/FileSystem.java
@@ -16,13 +16,13 @@ abstract class Resource implements resourceInterface {
     public abstract List<String> ls();
 
     public String getName() {
-        return "Name: " + name;
+        return name;
     }
 }
 
 class File extends Resource {
 
-    String content;
+    String content = "";
 
     public File(String name) {
         super(name);
@@ -46,7 +46,7 @@ class File extends Resource {
 
 class Directory extends Resource {
 
-    List<Resource> resources = new ArrayList<>();
+    HashMap<String, Resource> resources = new HashMap<>();
 
     public Directory(String name) {
         super(name);
@@ -55,16 +55,22 @@ class Directory extends Resource {
     @Override
     public List<String> ls() {
         List<String> ans =  new ArrayList<>();
-        for(Resource resource: resources) ans.add(resource.getName());
+        for(Resource resource: resources.values()) ans.add(resource.getName());
         return ans;
     }
 
-    public void touch(String name) {
-        resources.add(new File(name));
+    public File touch(String name) {
+        if(!resources.containsKey(name)) resources.put(name, new File(name));
+        return (File) resources.get(name);
     }
 
-    public void mkdir(String name) {
-        resources.add(new Directory(name));
+    public Directory mkdir(String name) {
+        if(!resources.containsKey(name)) resources.put(name, new Directory(name));
+        return (Directory) resources.get(name);
+    }
+
+    public Resource getResource(String name) {
+        return resources.get(name);
     }
 }
 
@@ -72,22 +78,81 @@ class FileSystem {
     Directory root = new Directory("~");
 
     public void mkdir(String path) {
+        List<String> resourceDirectoryPath = getResourceDirectory(path);
+        String resourceName = getResourceName(path);
 
+        Directory currentDir = root;
+
+        for(String resourceDirectory: resourceDirectoryPath) {
+            currentDir = currentDir.mkdir(resourceDirectory);
+        }
+
+        currentDir.mkdir(resourceName);
     }
 
-    public void touch(String path) {
+    public File touch(String path) {
+        List<String> resourceDirectoryPath = getResourceDirectory(path);
+        String resourceName = getResourceName(path);
 
+        Directory currentDir = root;
+
+        for(String resourceDirectory: resourceDirectoryPath) {
+            currentDir = currentDir.mkdir(resourceDirectory);
+        }
+
+        return currentDir.touch(resourceName);
     }
 
     public void addContent(String path, String content) {
+        File file = touch(path);
+        file.addContent(content);
+    }
+
+    public void ls(String path) {
+        System.out.println("------------------------------");
+        List<String> resourceDirectoryPath = getResourceDirectory(path);
+        String resourceName = getResourceName(path);
+
+        Directory currentDir = root;
+
+        for(String resourceDirectory: resourceDirectoryPath) {
+            currentDir = currentDir.mkdir(resourceDirectory);
+        }
+
+        Resource resource = resourceName.isEmpty() ? root : currentDir.getResource(resourceName);
+
+        for(String s: resource.ls()) {
+            System.out.println(s);
+        }
 
     }
 
-    public String ls(String path) {
-        return "";
+    public void getContent(String path) {
+        File file = touch(path);
+        System.out.println("------------------------------");
+        System.out.println(file.getContent());
     }
 
-    public String getContent(String path) {
+    private List<String> getResourceDirectory(String path) {
+        String[] components = path.split("/");
+        ArrayList<String> result = new ArrayList<>();
+        for (String component : components) {
+            if (!component.isEmpty()) {
+                result.add(component);
+            }
+        }
+        if(!result.isEmpty()) result.removeLast();
+        return result;
+    }
+
+    private String getResourceName(String path) {
+        String[] components = path.split("/");
+
+        for(int i = components.length - 1; i >= 0; i--){
+            if (!components[i].isEmpty()) {
+                return components[i];
+            }
+        }
         return "";
     }
 }
